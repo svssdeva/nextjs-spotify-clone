88549f239f4981e1edb964411189719725edf595
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2023-06-10 13:54:19 +0530

    added simple sidebar

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {twMerge} from "tailwind-merge";
 
 interface BoxProps {
     children: React.ReactNode;
@@ -9,7 +10,13 @@ const Box: React.FC<BoxProps> = ({
     className
                                  }) => {
   return (
-      <div>
+      <div className={twMerge(`
+      bg-neutral-900
+      rounded-lg
+      h-fit
+      w-full
+      `,
+          className)}>
           {children}
       </div>
   )
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Box from "@/components/Box";
+import SidebarItem from "@/components/SidebarItem";
 import {usePathname} from "next/navigation";
 import React, {useMemo} from "react";
 import {BiHome} from "react-icons/bi";
@@ -31,8 +32,17 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
       <div className="flex h-full">
           <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
              <Box>
+               <div className=" flex flex-col gap-y-4 px-5 py-4">
+                   {routes.map((route) => (
+                       <SidebarItem key={route.label} {...route}>
 
+                       </SidebarItem>
+                   ))}
+               </div>
              </Box>
+              <Box className="overflow-y-auto h-full">
+                song lib
+              </Box>
           </div>
       </div>
   )
